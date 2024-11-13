97c36aa8b8bb65f81bc1141e2e94f8c02c85fb69
CaioHPerlin <caiohperlin@gmail.com>
2024-11-13 11:19:02 -0400

    make second section properly responsive

--- src/components/LandingPageSections/SecondSection/index.tsx
+++ src/components/LandingPageSections/SecondSection/index.tsx
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion';
 const SecondSection = () => {
   return (
     <motion.section
-      className="container flex justify-center items-center h-[400px] mx-auto px-[20px] lg:px-[115px] mb-[120px]"
+      className="container flex justify-center items-center h-[100px] md:h-[400px] mx-auto px-5 lg:px-[115px] mb-12 md:mb-28"
       id="blue-section"
     >
-      <div className="mt-[-200px] max-w-[1100px] absolute">
+      <div className="-mt-[100px] md:-mt-[200px] max-w-[1100px] absolute">
         <img src="/second.png" alt="ProcStudio clients" />
       </div>
     </motion.section>
