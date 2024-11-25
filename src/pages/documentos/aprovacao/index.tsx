0fd95d66fe3e9f0ccb2c3dc3a562d8d60972b407
CaioHPerlin <caiohperlin@gmail.com>
2024-11-25 11:49:56 -0400

    feat: insert work data at the top of the page

--- src/pages/documentos/aprovacao/index.tsx
+++ src/pages/documentos/aprovacao/index.tsx
@@ -1,10 +1,14 @@
 import dynamic from 'next/dynamic';
 import { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Container, ContentContainer, PageTitle } from '../../../styles/globals';
+import { Container, PageTitle } from '../../../styles/globals';
 import { PageTitleContext } from '../../../contexts/PageTitleContext';
 import { Footer } from '../../../components';
 import { getWorkById } from '../../../services/works';
+import { Button, CircularProgress } from '@mui/material';
+import { IWorksListProps } from '../../../interfaces/IWork';
+import { PiSuitcase } from 'react-icons/pi';
+import { ContainerDetails, DetailsWrapper } from '../../../components/Details/styles';
 const Layout = dynamic(() => import('@/components/Layout'), { ssr: false });
 
 const DocumentApproval = () => {
@@ -13,7 +17,7 @@ const DocumentApproval = () => {
   const { id, client, responsible } = router.query;
 
   const [loading, setLoading] = useState(true);
-  const [workData, setWorkData] = useState<any>(null);
+  const [workData, setWorkData] = useState<IWorksListProps>({} as IWorksListProps);
 
   const fetchWorkData = async (workId: string) => {
     try {
@@ -48,6 +52,8 @@ const DocumentApproval = () => {
     }
   }, [id]);
 
+  const handleReturn = () => {};
+
   return (
     <>
       <Layout>
@@ -55,29 +61,182 @@ const DocumentApproval = () => {
           <div className="flex flex-row justify-between">
             <PageTitle showTitle={showTitle}>{'Revisão e Aprovação de Documentos'}</PageTitle>
           </div>
-          <ContentContainer>
-            {loading ? (
-              <p>Carregando...</p>
-            ) : workData ? (
-              <div>
-                <h1>Detalhes do Documento</h1>
-                <p>
-                  <strong>ID:</strong> {id}
-                </p>
-                <p>
-                  <strong>Cliente:</strong> {client || 'N/A'}
-                </p>
-                <p>
-                  <strong>Responsável:</strong> {responsible || 'N/A'}
-                </p>
-                <p>
-                  <strong>Dados do Trabalho:</strong> {JSON.stringify(workData, null, 2)}
-                </p>
-              </div>
-            ) : (
-              <p>Não foram encontrados dados para o trabalho.</p>
-            )}
-          </ContentContainer>
+          <Button
+            onClick={handleReturn}
+            variant={'contained'}
+            sx={{
+              height: '36px',
+              width: '100px',
+              textTransform: 'none',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            {'Voltar'}
+          </Button>
+          {loading ? (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '60vh',
+              }}
+            >
+              <CircularProgress />
+            </div>
+          ) : !workData ? (
+            <p>Não foram encontrados dados para o trabalho.</p>
+          ) : (
+            <DetailsWrapper
+              style={{
+                marginTop: 32,
+                borderBottom: '1px solid #C0C0C0',
+                boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.25)',
+              }}
+            >
+              <ContainerDetails
+                style={{
+                  gap: '18px',
+                }}
+              >
+                <>
+                  <div
+                    className="flex bg-white"
+                    style={{
+                      padding: '20px 32px 20px 32px',
+                      borderBottom: '1px solid #C0C0C0',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <div className="flex items-center gap-2">
+                      <PiSuitcase size={24} color="#344054" />
+
+                      <div className="w-[2px] bg-gray-300 h-8" />
+
+                      <span
+                        style={{
+                          fontSize: '22px',
+                          fontWeight: '500',
+                          color: '#344054',
+                        }}
+                      >
+                        Dados do Trabalho
+                      </span>
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '18px',
+                      paddingBottom: '20px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+                        gap: '18px',
+                        padding: '0 32px',
+                      }}
+                    >
+                      <div
+                        className="flex"
+                        style={{
+                          flexDirection: 'column',
+                          gap: '8px',
+                          alignItems: 'flex-start',
+                        }}
+                      >
+                        <span
+                          style={{
+                            color: '#344054',
+                            fontSize: '20px',
+                            fontWeight: '500',
+                          }}
+                        >
+                          Cliente:
+                        </span>
+                        <span
+                          style={{
+                            fontSize: '18px',
+                            color: '#344054',
+                            fontWeight: '400',
+                          }}
+                        >
+                          {client}
+                        </span>
+                      </div>
+
+                      <div className="flex flex-col gap-[8px] items-start">
+                        <span
+                          style={{
+                            color: '#344054',
+                            fontSize: '20px',
+                            fontWeight: '500',
+                          }}
+                        >
+                          Responsável:
+                        </span>
+                        <span
+                          style={{
+                            fontSize: '18px',
+                            color: '#344054',
+                            fontWeight: '400',
+                          }}
+                        >
+                          {responsible}
+                        </span>
+                      </div>
+
+                      <div className="flex flex-col gap-[8px] items-start">
+                        <span
+                          style={{
+                            color: '#344054',
+                            fontSize: '20px',
+                            fontWeight: '500',
+                          }}
+                        >
+                          CNPJ
+                        </span>
+                        <span
+                          style={{
+                            fontSize: '18px',
+                            color: '#344054',
+                            fontWeight: '400',
+                          }}
+                        >
+                          {workData.attributes.number}
+                        </span>
+                      </div>
+
+                      <div className="flex flex-col gap-[8px] items-start">
+                        <span
+                          style={{
+                            color: '#344054',
+                            fontSize: '20px',
+                            fontWeight: '500',
+                          }}
+                        >
+                          Data de criação do trabalho:
+                        </span>
+                        <span
+                          style={{
+                            fontSize: '18px',
+                            color: '#344054',
+                            fontWeight: '400',
+                          }}
+                        >
+                          {'XX/XX/XXXX'}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              </ContainerDetails>
+            </DetailsWrapper>
+          )}
         </Container>
         <Footer />
       </Layout>
