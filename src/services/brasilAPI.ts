4a43329267b3b62a0e5dcc286c141487c767d8da
CaioHPerlin <caiohperlin@gmail.com>
2025-02-04 15:35:29 -0400

    feat: implement reduce paramenter on getAllBanks function

--- src/services/brasilAPI.ts
+++ src/services/brasilAPI.ts
@@ -1,3 +1,4 @@
+import { IBank } from '@/interfaces/IBank';
 import axios from 'axios';
 const URL = 'https://brasilapi.com.br/api/';
 
@@ -10,10 +11,38 @@ const getCEPDetails = async (cep: string) => {
   }
 };
 
-const getAllBanks = async () => {
+const getAllBanks = async (reduce: boolean = true) => {
   try {
     const response = await axios.get(`${URL}banks/v1`);
-    return response.data;
+    const banks: IBank[] = response.data;
+
+    if (!reduce) {
+      return banks;
+    }
+
+    const uniqueBanks: IBank[] = [];
+    banks.forEach(bank => {
+      const nameToNormalize = bank.name || bank.fullName;
+      if (!nameToNormalize) {
+        return;
+      }
+
+      if (nameToNormalize.toLowerCase() === 'selic' || nameToNormalize.toLowerCase() === 'Bacen') {
+        return;
+      }
+
+      const normalizedName = nameToNormalize
+        .replace(/S\.A\.?/gi, '') // Remove "S.A." and "S.A"
+        .replace(/FINANCIAMENTOS|BBI|BERJ/gi, '') // Remove unintelligible acronyms
+        .replace(/\bBCO\b/gi, 'BANCO') // Replace "BCO" with "BANCO"
+        .trim();
+
+      if (!uniqueBanks.some(b => b.name === normalizedName)) {
+        uniqueBanks.push({ ...bank, name: normalizedName });
+      }
+    });
+
+    return uniqueBanks;
   } catch (error) {
     throw error;
   }
