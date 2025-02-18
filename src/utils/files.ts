e271e726bd2575b7290893a29f451b0c34fac931
CaioHPerlin <caiohperlin@gmail.com>
2025-02-18 15:25:35 -0400

    feat: check if file exists before attempting download

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,13 +2,19 @@ export const openFileInNewTab = (url: string) => {
   window.open(url, '_blank');
 };
 
-export const downloadS3FileByUrl = (url: string) => {
-  console.log('Downloading file:', url);
+export const downloadS3FileByUrl = async (url: string, filename?: string) => {
+  if (!url) {
+    return console.error('File URL is null.');
+  }
+
+  const lowerCaseUrl = url.toLowerCase();
+
+  if (lowerCaseUrl.includes('.pdf')) {
+    return openFileInNewTab(url);
+  }
 
   const link = document.createElement('a');
   link.href = url;
-  link.setAttribute('download', ''); // Forces download behavior
-  document.body.appendChild(link);
+  link.download = filename || '';
   link.click();
-  document.body.removeChild(link);
 };
